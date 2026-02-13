aa30d17ebe478631da4d19b5d44d77b8e794be8e
Harinish Sabapathi <harinishsv@gmail.com>
2026-02-13 10:36:02 +0530

    Cleanup: Keep only new Java problems in master

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-        scanner.close();
-    }
-}
