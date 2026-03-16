e66e766fb9398326a13e9bfdbd3bbc37a691ddc2
Student <student@example.com>
2026-03-16 09:25:07 +0530

    Initial commit - Use Case 1: Application Entry

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,13 @@
+/**
+ * UseCase1HotelBookingApp
+ * Demonstrates application entry and welcome message
+ * 
+ * @author Student
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("Use Case 1: Application Entry & Welcome Message");
+        System.out.println("Welcome to Hotel Booking System v1.0");
+    }
+}
